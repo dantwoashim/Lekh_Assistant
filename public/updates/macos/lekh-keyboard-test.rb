7b6b89deca756c8868d2a06af13678e836909bc9
dantwoashim <twodan033@gmail.com>
2026-07-17 10:24:39 +0545

    Publish build 172 File Provider-safe installer

--- public/updates/macos/lekh-keyboard-test.rb
+++ public/updates/macos/lekh-keyboard-test.rb
@@ -1,6 +1,6 @@
 cask "lekh-keyboard-test" do
-  version "0.1.0,170"
-  sha256 "4a22efefefcae0712d272b4e04b4c3b83d1b2371237bbe47c5e0b2f9362e5c79"
+  version "0.1.0,172"
+  sha256 "f969229e4790d77cca59d8d49b88fd083c66dc47dcbd31a34a2b8c63f70429ad"
 
   url "https://lekh-assistant.pages.dev/updates/macos/Lekh-Keyboard-Test-Installer.zip"
   name "Lekh Keyboard"
